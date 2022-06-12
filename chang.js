1ad6180fc49d626f7c2306f4c5f292f510cfbb43
oaoo430 <62593836+KimMyeongjae@users.noreply.github.com>
2022-06-12 21:19:36 +0900

    update sheet

--- chang.js
+++ chang.js
@@ -1,15 +1,30 @@
-const sheetId = "1l4fgMDc0htCgCqC-OHq5gbP1o9K_-z6NLLQOJfZqSOU";
-const base = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?`;
-const date = new Date();
-const sheetName = `${date.getFullYear()}년`;
-const query = encodeURIComponent("Select *");
-const url = `${base}&sheet=${sheetName}&tq=${query}`;
-const data = [];
 document.addEventListener("DOMContentLoaded", init);
 
 const output = document.querySelector(".output");
+const combo = document.querySelector(".combo");
+const sheetId = "1l4fgMDc0htCgCqC-OHq5gbP1o9K_-z6NLLQOJfZqSOU";
+let year = combo.value;
+let base = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?`;
+let sheetName = `${year}년`;
+let query = encodeURIComponent("Select *");
+let url = `${base}&sheet=${sheetName}&tq=${query}`;
+let data = [];
+
+const setUrl = () => {
+  data = [];
+  year = combo.value;
+  sheetName = `${year}년`;
+  url = `${base}&sheet=${sheetName}&tq=${query}`;
+};
+
+function handleOnChange(e) {
+  // 선택된 데이터 가져오기
+  output.innerHTML = "";
+  init();
+}
 
 function init() {
+  setUrl();
   fetch(url)
     .then((res) => res.text())
     .then((rep) => {
@@ -21,8 +36,9 @@ function init() {
       //Extract column labels
 
       jsonData.table.cols.forEach((heading, idx) => {
-        if (idx === 13) return;
+        if (idx > 12) return;
         let column = heading.label;
+
         colz.push(column);
         const th = document.createElement("th");
 
@@ -37,17 +53,29 @@ function init() {
       });
       output.appendChild(tr);
 
-      const rmk = jsonData.table.rows[12].c[13].v;
-      console.log(rmk);
+      let rmk = jsonData.table.rows.filter((value, idx) => idx > 11);
+      let remark = {};
+      remark = rmk.map((r) => r.c[13].v);
+      remark = remark.filter((r) => r != "비고");
+
+      rmk = remark.join("\n");
+
+      // jsonData.table.rows[12].c[13].v;
 
       //extract row data:
       jsonData.table.rows.forEach((rowData, idx) => {
-        if (idx === 11) return;
+        if (idx == 0 || idx > 10) {
+          return;
+        }
+
         const row = {};
         colz.forEach((ele, ind) => {
           row[ele] = rowData.c[ind] != null ? rowData.c[ind].v : "";
-          if (row[ele] === null || row[ele] === undefined || row[ele] === 0)
-            row[ele] = "";
+
+          if (row[ele] === null || row[ele] === undefined) row[ele] = "";
+          if (row[ele] === 0) {
+            row[ele] = "O";
+          }
           if (typeof row[ele] === "number") {
             row[ele] = row[ele].toLocaleString("ko-KR");
           }
@@ -60,7 +88,6 @@ function init() {
       const h = document.querySelector(".h");
       const r = document.querySelector(".rmk");
       r.innerText = rmk;
-      h.innerText = date.getFullYear();
     });
 
   // document.querySelector("th").innerText = 100000000;
@@ -74,8 +101,12 @@ function processRows(json) {
 
     keys.forEach((key) => {
       const td = document.createElement("td");
+      if (row[key] === "O") {
+        td.style.textAlign = "center";
+      }
       td.style.border = "5px";
       td.textContent = row[key];
+
       tr.appendChild(td);
     });
     output.appendChild(tr);
